59ad9722f6fdb3724eb57efcc5b1b228f9c91bd8
unknown <carteaga460@gmail.com>
2022-07-20 10:15:47 -0400

    basic schema

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,20 @@
 'use strict';
+const mongoose = require('mongoose');
+
+const issueSchema = new mongoose.Schema({
+  'issue_title': {type: String, required: true},
+  'issue_text': {type: String, required: true},
+  'created_by': {type: String, required: true},
+  'assigned_to': String,
+  'status_text': String
+});
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      let project = req.params.project;
-      let {open} = req.query;
+      console.log(req);
 
     })
     
