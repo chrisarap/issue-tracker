1a22d01d958d429c71421f9143436770a33b9b29
chrisarap <carteaga460@gmail.com>
2022-07-20 20:55:03 -0400

    adding first test

--- routes/api.js
+++ routes/api.js
@@ -1,40 +1,61 @@
 'use strict';
 const mongoose = require('mongoose');
 
-const issueSchema = new mongoose.Schema({
-  'issue_title': {type: String, required: true},
-  'issue_text': {type: String, required: true},
-  'created_by': {type: String, required: true},
-  'assigned_to': String,
-  'status_text': String
-});
-
 module.exports = function (app) {
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      console.log(req);
+  mongoose.connect(process.env.MONGO_URI);
+  const issueSchema = new mongoose.Schema({
+     "issue_title": {type: String, required: true},
+     "issue_text": {type: String, required: true},
+     "created_on": Date,
+     "updated_on": Date,
+     "created_by": {type: String, required: true},
+     "assigned_to": String,
+     "open": Boolean,
+     "status_text": String,
+  });
 
+  const Issue = mongoose.model('Issue', issueSchema);
+
+  app.route('/api/issues/:project')
+
+    .get(function (req, res){
+      let project = req.params.project;
+      let {open} = req.query;
+      Issue.find({}, (err, data) => {
+        res.json(data);
+      })
     })
-    
+
     .post(function (req, res){
       let project = req.params.project;
-
+      const {issue_title, issue_text, created_on, updated_on, created_by, assigned_to, open, status_text} = req.body;
+      Issue.create({
+        issue_title,
+        issue_text,
+        created_on,
+        updated_on,
+        created_by,
+        assigned_to,
+        open,
+        status_text
+      }, (err, data) => {
+        if (err) return console.error(err._message);
+        console.log(req.body.error)
+      });
     })
-    
+
     .put(function (req, res){
       let project = req.params.project;
-      
+
     })
-    
+
     .delete(function (req, res){
       let project = req.params.project;
-      
+      let { id } = req.body._id;
+
+      Issue.deleteOne(id, (err, data) => {
+        if (err) return console.error(err);
+      });
     });
-  
-  app.route('/api/issues/apitest')
-    .post((req, res) => {})
-    .put((req, res) => {})
-    .delete((req, res) => {});
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,57 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  test("create issue with every field /api/issues/{project}", done => {
+    chai
+        .request(server)
+        .post('/api/issues/apitest')
+        .send({
+            issue_title: "test",
+            issue_text: "it just a test",
+            created_by: "max",
+            assigned_to: "maxi",
+            status_text: "working"
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.issue_title, "test");
+          assert.equal(res.body.issue_text, "it just a test");
+          assert.equal(res.body.created_by, "max");
+          assert.equal(res.body.assigned_to, "maxi");
+          assert.equal(res.body.status_text, "working");
+        });
+        done();
+  });
+
+  test('create issue with every field /api/issues/{project}', done => {
+    chai
+      .request(server)
+      .post('/api/issues/apitest')
+      .send({
+        issue_title: "test",
+        issue_text: "it just a test",
+        created_by: "max",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.issue_title, "test");
+        assert.equal(res.body.issue_text, "it just a test");
+        assert.equal(res.body.created_by, "max");
+      })
+      done();
+  });
+
+  test('create issue with missing required field /api/issues/{project}', done => {
+    chai
+      .request(server)
+      .post('/api/issues/apitest')
+      .send({
+        issue_title: "test"
+      })
+      .end((err, res) => {
+          assert.equal(res.status, 200, "200 status");
+          assert.ifError(err);
+      })
+      done();
+  });
 });
